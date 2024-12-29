5841f4747c6f0f314330e50de88df1a203567864
Carl Ryden <caryden@alum.mit.edu>
2024-12-29 13:44:18 -0500

    Added kotlin support.  basic bones in place.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BucketPathStates.kt
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto
+
+enum class BucketPathStates {
+    START,
+    SCORING_PRELOAD,
+    PICKUP_SAMPLE_1,
+    SCORING_SAMPLE_1,
+    PICKUP_SAMPLE_2,
+    SCORING_SAMPLE_2,
+    PICKUP_SAMPLE_3,
+    SCORING_SAMPLE_3,
+    PARKING
+}
